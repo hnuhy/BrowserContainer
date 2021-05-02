fd846691c7f444528ba71f65117e578326e61ba7
黄毅 <huangyix@hnu.edu.cn>
2021-05-02 21:08:06 +0800

    初始化最后一个支持XP的CefSharp，版本：49.0.1

---
+++ Framework/CefSharp/CefSharp/AddressChangedEventArgs.cs
@@ -0,0 +1,28 @@
+﻿// Copyright © 2010-2016 The CefSharp Authors. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
+
+using System;
+
+namespace CefSharp
+{
+    /// <summary>
+    /// Event arguments to the AddressChanged event handler.
+    /// </summary>
+    public class AddressChangedEventArgs : EventArgs
+    {
+        public IBrowser Browser { get; set; }
+        public string Address { get; private set; }
+
+        /// <summary>
+        /// Called when a frame's address has changed. 
+        /// </summary>
+        /// <param name="browser">the browser object</param>
+        /// <param name="address">the address</param>
+        public AddressChangedEventArgs(IBrowser browser, string address)
+        {
+            Browser = browser;
+            Address = address;
+        }
+    }
+}
---
+++ Framework/CefSharp/CefSharp/ICookieVisitor.cs
@@ -0,0 +1,15 @@
+﻿// Copyright © 2010-2016 The CefSharp Authors. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
+
+namespace CefSharp
+{
+    /// <summary>
+    /// Interface to implement for visiting cookie values. 
+    /// The methods of this class will always be called on the IO thread.
+    /// </summary>
+    public interface ICookieVisitor
+    {
+        bool Visit(Cookie cookie, int count, int total, ref bool deleteCookie);
+    }
+}
